a559378cb32b1f350fac3e2488b6469e02f175f8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-22 07:33:56 +0300

    🚩 FEAT: add Resort class
    Signed-off-by: Maftun Hashimli <maftunhashimli@gmail.com>

---
+++ src/com/java/data/Resort.java
@@ -0,0 +1,57 @@
+package data;
+
+import java.math.BigDecimal;
+
+public class Resort extends Home {
+    public Resort(BigDecimal price, int squareMeters, int roomNumber, int saloonNumber) {
+        super(price, squareMeters, roomNumber, saloonNumber);
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setPrice(BigDecimal price) {
+        super.setPrice(price);
+    }
+
+    @Override
+    public int getSquareMeters() {
+        return super.getSquareMeters();
+    }
+
+    @Override
+    public void setSquareMeters(int squareMeters) {
+        super.setSquareMeters(squareMeters);
+    }
+
+    @Override
+    public int getRoomNumber() {
+        return super.getRoomNumber();
+    }
+
+    @Override
+    public void setRoomNumber(int roomNumber) {
+        super.setRoomNumber(roomNumber);
+    }
+
+    @Override
+    public int getSaloonNumber() {
+        return super.getSaloonNumber();
+    }
+
+    @Override
+    public void setSaloonNumber(int saloonNumber) {
+        super.setSaloonNumber(saloonNumber);
+    }
+
+    @Override
+    public String toString() {
+        return ("\n" + "Price:" + this.getPrice() +
+                " Room Number " + this.getRoomNumber() +
+                " Area " + this.getSquareMeters() +
+                " Saloon Number : " + this.getSaloonNumber());
+    }
+}
